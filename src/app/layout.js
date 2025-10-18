927b2d16bf84d3b9b420ba18c43c2c9bc2914eab
ANGELOREYES-afk <angelohenaoreyes@yahoo.com>
2025-10-18 16:22:06 -0500

    everything

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <AuthProvider>
         {children}
-        </AuthProvider>
       </body>
     </html>
   );
